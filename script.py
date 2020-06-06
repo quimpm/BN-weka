b87b9ec7abddd1827bd23a2f9a43fed0e537116e
Quim Picó Mora <quimpm99@gmail.com>
2020-06-06 19:46:33 +0200

    splited data in learning data and evaluation data

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import csv
 import sys
 import math
+import random
+
+DNI_DIGITS = 83707
 
 """ This function ensures a good usage of the script """
 def param_treatment(args):
@@ -14,20 +17,35 @@ def get_data(filename):
     data = []
     with open(filename) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
-        for entries,row in enumerate(csvReader):
+        for row in csvReader:
             data.append(row)
-    return data, entries
+    return data
 
-def make_dataset_partition(data, entries, division):
-    pass
+def make_dataset_partition(data_set, entries, division):
+    random.seed(DNI_DIGITS)
+    n = 0
+    learning_data = []
+    evaluation_data = []
+    for i in range(entries):
+        if n < division:
+            index = random.randint(0,len(data_set)-1)
+            elem = data_set.pop(index)
+            learning_data.append(elem)
+        else:
+            evaluation_data = data_set
+        n +=1
+    return learning_data, evaluation_data
+
 
 def main():
     """Read Data"""
     filename, learning_file, evaluation_file = param_treatment(sys.argv)
-    data, entries = get_data(filename)
+    data = get_data(filename)
+    entries = len(data)
     division = entries//4 * 3
+
     "Get divisions of data"  
-    learing_data, evaluation_data = make_dataset_partition(division, data)
+    learning_data, evaluation_data = make_dataset_partition(data, entries, division)
 
 if __name__=="__main__":
     main()
